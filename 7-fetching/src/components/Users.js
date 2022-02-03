98668ea81ae52e1b629f9c0b0119f7800012f93d
tayfundagci <tayfundagci3517@gmail.com>
2022-02-03 17:30:11 +0300

    an overview and react router

--- 7-fetching/src/components/Users.js
+++ 7-fetching/src/components/Users.js
@@ -1,30 +1,46 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'; 
 import axios from 'axios'
 
 function Users() {
-
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         axios('https://jsonplaceholder.typicode.com/users')
-        // .then((res) => res.json()) //axios ile yapınca buna gerek yok (npm add axios)
-        .then((res) => setUsers(res.data))
+        .then((res) => setUsers(res.data)) // ekranda göstermek için useState kullan
         .catch((e) => console.log(e))
-        .finally(() => setIsLoading(false)) // Loadingi yok etmek için
+        .finally(() => setIsLoading(false))
+
     }, [])
 
-    return (
+  return (
+    <div>
+    <h1>Users</h1>
+
+    {isLoading && <div>Loading...</div>}
         <div>
-            <h1>Users</h1>
-
-            { isLoading && <div>Loading...</div>} 
-
             {
-                users.map((user) => <div key={user.id}>{user.name}</div>)
+                <div>
+                {
+                    users.map((user) => ( 
+                        <li key={user.id}>
+
+                            <span className='test'>ID</span> = {user.id} <br /> 
+                            <span className='test'>Name</span> = {user.name} <br /> 
+                            <span className='test'>Username</span> = {user.username} <br /> 
+                            <span className='test'>Email</span> = {user.email} <br /> 
+                            <span className='test'>Phone</span> = {user.phone} <br />
+                            <span className='test'>Website</span> = {user.website} <br /> <br />
+
+                        </li>
+                         ))
+                }
+                </div>
             }
         </div>
-    )
+    </div>
+  );
 }
 
-export default Users
+export default Users;
+    
