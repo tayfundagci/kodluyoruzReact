66d2ae81017e5a4c6be490deddbe466aa35ecf95
tayfundagci <tayfundagci3517@gmail.com>
2022-02-10 11:26:46 +0300

    initial

---
+++ 17-deploying/netlify/src/components/User.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import {useParams, Link} from 'react-router-dom';
+import axios from 'axios';
+// ilk olarak projeye npm add axios yapılmalı.
+function User() {
+    const [loading, setLoading] = useState(true) // Loading...
+    const [user, setUser] = useState({});
+
+    const { id } = useParams();
+ 
+    useEffect(() => {
+        axios(`https://jsonplaceholder.typicode.com/users/${id}`) // api den veri bu şekilde çekilir. 
+        .then(res => setUser(res.data)).finally(() => setLoading(false)) // Loadingin gitmesini sağlar
+    }, [id]); // next user'daki user bilgilerinin dğeiişmesi için buraya id eklendi.
+
+    return <div>
+        <h1>User Detail</h1>
+        {loading && <div>Loading...</div>} {/* loading varsa loading yazdır. */}
+        {!loading && <code>
+            {JSON.stringify(user)}
+        </code>}
+            <br /><br />
+        <Link to={`/users/${parseInt(id)+1}`}>
+            Next User ({parseInt(id)+1}) 
+            </Link>
+    </div>;
+}
+
+export default User;
