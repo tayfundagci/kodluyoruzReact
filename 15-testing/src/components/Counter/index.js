f92567931e74c4e8c3c038f12d55870e907328ef
tayfundagci <tayfundagci3517@gmail.com>
2022-02-18 11:28:45 +0300

    test

--- 15-testing/src/components/Counter/index.js
+++ 15-testing/src/components/Counter/index.js
@@ -5,8 +5,8 @@ function Counter() {
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decreasee</button>
+      <button onClick={() => setCount(count + 1)}>Increasee</button>
     </div>
   );
 }
